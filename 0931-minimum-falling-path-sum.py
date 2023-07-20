a08af1edf32b2d84635e2850cc67a5d1710159f1
duressa-feyissa <101529984+duressa-feyissa@users.noreply.github.com>
2023-07-20 16:32:24 +0300

    Update 0931-minimum-falling-path-sum.py

--- 0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        N = len(matrix)
-        memory = [0] * N
+        length = len(matrix)
+        memory = [0] * length
         for i in range(N-1,-1,-1):
-            next = [0] * N
-            for j in range(N):
+            next = [0] * length
+            for j in range(length):
                 Min = float('inf')
                 if j - 1 > -1:
                     Min = min(memory[j - 1], Min)
@@ -14,7 +14,3 @@ class Solution:
                 next[j] = Min + matrix[i][j]
             memory = next
         return min(memory)
-                    
-                    
-                    
-                
